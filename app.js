a2c5bfb1f37cedbadc65d9c4ddc55cf1aa4f7747
whywaita <whywaita@whywrite.it>
2017-11-16 13:10:53 +0900

    add profile image (for tech , app computed func)

--- app.js
+++ app.js
@@ -17,6 +17,22 @@ const app = new Vue({
       let url = buildUrl(section);
       axios.get(url)
       .then(response => { this.results = response.data })
+      .catch( error => { console.log(error); });
+    }
+  },
+  computed: {
+    processedPosts() {
+      let posts = this.results;
+
+      posts.map(post => {
+        post.image_url = post.image_url;
+      });
+
+      let i, j, chunkedArray = [], chunk = 4;
+      for (i=0, j=0; i < posts.length; i += chunk, j++) {
+        chunkedArray[j] = posts.slice(i,i+chunk);
+      }
+      return chunkedArray;
     }
   }
 });
