ea669d42903394c48d288429f6e562d2de10bef5
whywaita <whywaita@whywrite.it>
2017-11-16 17:08:09 +0900

    display rendered_body -> outline

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ function buildUrl (section) {
   return QiitaBaseUrl + section
 };
 
+function cutSection (body) {
+  return body.substr(0, 20)
+};
+
+function cutTitle (title) {
+  if (title.length - 1 <= 50) {
+    return title
+  }
+  return title.substr(0, 49) + "..."
+};
+
 const app = new Vue({
   el: '#app',
   data: {
@@ -17,7 +28,7 @@ const app = new Vue({
       let url = buildUrl(section);
       axios.get(url)
       .then(response => { this.results = response.data })
-      .catch( error => { console.log(error); });
+      .catch(error => { console.log(error);});
     }
   },
   computed: {
@@ -26,7 +37,10 @@ const app = new Vue({
 
       posts.map(post => {
         post.image_url = post.user.profile_image_url;
+        post.outline = cutSection(post.rendered_body);
+        post.title = cutTitle(post.title);
       });
+
 
       let i, j, chunkedArray = [], chunk = 4;
       for (i=0, j=0; i < posts.length; i += chunk, j++) {
